b2e560fea4e3a7cd35bc21542a500291bdc18ead
sandpitt-turtle <wottkowicz@gmail.com>
2024-12-17 12:17:47 -0500

    didnt get too far but might as well update

--- froyo.js
+++ froyo.js
@@ -1,15 +1,21 @@
-// A visitor receives a prompt upon opening the website to enter a list of comma-separated 
-// froyo flavors. They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. 
-// When they view the browser console, they observe a table listing how many of each flavor they have 
-// ordered. In this case, they will be able to observe that they have ordered three vanilla, two 
+// A visitor receives a prompt upon opening the website to enter a list of comma-separated
+// froyo flavors. They type vanilla,vanilla,vanilla,strawberry,coffee,coffee.
+// When they view the browser console, they observe a table listing how many of each flavor they have
+// ordered. In this case, they will be able to observe that they have ordered three vanilla, two
 // coffee, and one strawberry froyo.
 
-
-
 // INPUT: vanilla,vanilla,vanilla,strawberry,coffee,coffee
-// OUTPUT: {vanilla:3, strawberry: 1 , coffee: 2}
+// OUTPUT: {vanilla: 3, strawberry: 1 , coffee: 2}
 
-let flavors = window.prompt("Enter your flavors below")
+let flavors = window.prompt("Enter your flavors below"); //prompt for flavor input
+let flavorArray 
 
-console.log(flavors)
+//
+// const arr = ["Vanilla, Vanilla, Vanilla, Stawberry, Coffee, Coffee"];
+//
 
+// const iceCream = {
+//   flavors: ["vanilla", "strawberry", "coffee"],
+// };
+
+// console.log(Object.keys(iceCream));
