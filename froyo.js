e4e5db78651f326d837f4c1e08b1ffedf65b887f
sandpitt-turtle <wottkowicz@gmail.com>
2024-12-18 18:43:33 -0500

    update

--- froyo.js
+++ froyo.js
@@ -7,10 +7,26 @@
 // INPUT: vanilla,vanilla,vanilla,strawberry,coffee,coffee
 // OUTPUT: {vanilla: 3, strawberry: 1 , coffee: 2}
 
-let flavors = window.prompt("Enter your flavors below"); //prompt for flavor input
-let flavorArray 
+//PROMPT USER FOR INPUT:
+let flavors = window.prompt("Enter your flavors below");
+flavors: ["vanilla", "strawberry", "coffee"];
+//SPLIT THE ARRAY
+let flavorsArray = flavors.split(",");
+console.log(flavorsArray);
 
-//
+//USE OBJECT TO STORE THE COUNTS
+const flavorCounts = {};
+
+//LOOP THROUGH ARRAY & SINGLE INCREMENT FOR EACH COUNT
+flavorsArray.forEach(function (flavor) {
+  if (flavor) {
+    flavorCounts[flavor] = (flavorCounts[flavor] || 0) + 1;
+  }
+});
+
+//PRINT/DISPLAY
+console.table(flavorCounts);
+
 // const arr = ["Vanilla, Vanilla, Vanilla, Stawberry, Coffee, Coffee"];
 //
 
